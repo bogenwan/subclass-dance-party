e222ff2587fa86a95d135108f4c2b859546563b7
bogenwan <pobo1230@gmail.com>
2016-12-19 16:50:39 -0800

    added bouncy dancer:

---
+++ src/bouncyDancer.js
@@ -0,0 +1,44 @@
+// var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
+//   var blinkyDancer = new makeDancer(top, left, timeBetweenSteps);
+
+//   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
+//   // so we must keep a copy of the old version of this function
+
+//   var oldStep = blinkyDancer.step;
+
+//   blinkyDancer.step = function() {
+//     // call the old version of step at the beginning of any call to this new version of step
+//     oldStep();
+//     // toggle() is a jQuery method to show/hide the <span> tag.
+//     // See http://api.jquery.com/category/effects/ for this and
+//     // other effects you can use on a jQuery-wrapped html tag.
+//     blinkyDancer.$node.toggle();
+//   };
+
+//   return blinkyDancer;
+// };
+
+
+
+
+
+var makeBouncyDancer = class extends makeDancer {
+  constructor(top, left, timeBetweenSteps) {
+
+    super(top, left, timeBetweenSteps);
+    // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
+    // so we must keep a copy of the old version of this function
+    this.oldStep = super.step;
+    this.step = this.newStep;
+  }
+
+  newStep() {
+    // call the old version of step at the beginning of any call to this new version of step
+    this.oldStep();
+    // toggle() is a jQuery method to show/hide the <span> tag.
+    // See http://api.jquery.com/category/effects/ for this and
+    // other effects you can use on a jQuery-wrapped html tag.
+    this.$node.addClass("animated infinite bounce");
+
+  }
+};
