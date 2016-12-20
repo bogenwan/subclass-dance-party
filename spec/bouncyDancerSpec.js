4ed3bc30d95bf4ad1f35cd6806afe4701da27427
bogenwan <pobo1230@gmail.com>
2016-12-19 17:04:26 -0800

    implement test for bouncyDancer

---
+++ spec/bouncyDancerSpec.js
@@ -0,0 +1,33 @@
+describe('bouncyDancer', function() {
+
+  var bouncyDancer, clock;
+  var timeBetweenSteps = 100;
+
+  beforeEach(function() {
+    clock = sinon.useFakeTimers();
+    bouncyDancer = new makeBouncyDancer(10, 20, timeBetweenSteps);
+  });
+
+  it('should have a jQuery $node object', function() {
+    expect(bouncyDancer.$node).to.be.an.instanceof(jQuery);
+  });
+
+  describe('dance', function() {
+    it('should call step at least once per second', function() {
+      sinon.spy(bouncyDancer, 'step');
+      expect(bouncyDancer.step.callCount).to.be.equal(0);
+      clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
+      clock.tick(timeBetweenSteps);
+
+      expect(bouncyDancer.step.callCount).to.be.equal(1);
+
+      clock.tick(timeBetweenSteps);
+      expect(bouncyDancer.step.callCount).to.be.equal(2);
+    });
+  });
+  describe('bounce', function() {
+    it('should have a class bounce', function() {
+      expect(bouncyDancer.$node.hasClass('bounce'));
+    });
+  });
+});
